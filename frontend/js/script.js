54e7f837cbb4c073b0a4eebd05ee6b4587091b7f
vamcrizer <vamcrizer@gmail.com>
2025-04-14 11:32:09 +0700

    Inference added via analyzeCode() in js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -158,6 +158,17 @@ document.addEventListener('DOMContentLoaded', function() {
         chatMessages.appendChild(analyzingDiv);
         chatMessages.scrollTop = chatMessages.scrollHeight;
         
+        try {
+            const response = await fetch("http:127.0.0.1:8000/generate",{
+                method: "POST",
+                headers : {"Content-Type" : "application/json"},
+                body: JSON.stringify({code})
+            });
+        
+        if (!response.ok){
+            throw new Error("Failed to analyze code.");
+        }
+        }
         // Simulate analysis after delay
         setTimeout(() => {
             // Add analysis complete message
