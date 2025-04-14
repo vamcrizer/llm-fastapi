bea30cd9a329cf30107479e65dc51ee8ba81af83
vamcrizer <vamcrizer@gmail.com>
2025-04-14 12:01:22 +0700

    Updated CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,17 @@
 import ollama
 from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class CodeAnalysisRequest(BaseModel):
     code: str
